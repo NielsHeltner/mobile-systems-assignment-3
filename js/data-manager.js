15fc2c0ba175ad6be5cee0c9af172fe57291dff0
Niels Heltner <niels.heltner@hotmail.com>
2018-10-17 15:21:29 +0200

    sampling is no longer done in both directions (only backwards now) and the filtered result is no longer inserted into the array getting samp

--- js/data-manager.js
+++ js/data-manager.js
@@ -3,7 +3,7 @@ let coords;
 let fileUpload = document.getElementById('fileUpload');
 fileUpload.onchange = parseFile;
 let inputSampleSize = document.getElementById('inputSampleSize');
-inputSampleSize.value = 2;
+inputSampleSize.value = 5;
 inputSampleSize.onchange = updateFilters;
 let inputThreshold = document.getElementById('inputThreshold');
 inputThreshold.value = 0.00001;
@@ -47,35 +47,31 @@ function filterMean() {
         let sum = sampleArray.reduce((a, b) => a + b, 0);
         let mean = sum / sampleArray.length;
         return mean;
-    });//biking = 8, waling = 50+
+    });
 }
 
 function filterMedian() {
     return filterForEach(coords['phone'], function (sampleArray) {
         sampleArray.sort();
-        var half = Math.floor(sampleArray.length / 2);
+        let half = Math.floor(sampleArray.length / 2);
         if (sampleArray.length % 2) {
             return sampleArray[half];
         }
-        else
-            return (sampleArray[half - 1] + sampleArray[half]) / 2.0;
+        return (sampleArray[half - 1] + sampleArray[half]) / 2.0;
     });
 }
 
 function filterForEach(originalCoords, filter) {
-    let filteredCoords = JSON.parse(JSON.stringify(originalCoords));
-    for (let column = 0; column < filteredCoords[0].length; column++) {
-        for (let row = 0; row < filteredCoords.length; row++) {
+    let filteredCoords = JSON.parse(JSON.stringify(originalCoords)); // for copying the 2d structure
+    for (let column = 0; column < originalCoords[0].length; column++) { // columns in outer loop, since we process all x values then all y values
+        for (let row = sampleSize; row < originalCoords.length; row++) { // start row at sample size, since we only sample backwards
             let sampleArray = [];
-            for (let rowOffset = row - sampleSize < 0 ? 0 : row - sampleSize; rowOffset < row + sampleSize && rowOffset < filteredCoords.length; rowOffset++) {
-                sampleArray.push(filteredCoords[rowOffset][column]);
+            for (let rowOffset = row - sampleSize; rowOffset <= row; rowOffset++) { // rowOffset can never be smaller than 0
+                sampleArray.push(originalCoords[rowOffset][column]);
             }
 
             let filterValue = filter(sampleArray);
-            let originalValue = filteredCoords[row][column];
-            if (Math.abs(originalValue - filterValue) > threshold) {
-                filteredCoords[row][column] = filterValue;
-            }
+            filteredCoords[row][column] = filterValue;
         }
     }
     return filteredCoords;
